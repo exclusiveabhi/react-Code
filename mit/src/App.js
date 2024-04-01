16758720959919153c2e55b1bc5a29dd6404ead1
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-04-01 16:27:02 +0530

    01/04/2024

---
+++ mit/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+           <code></code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
