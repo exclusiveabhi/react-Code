26ffae68375555147e1eb85b675843a37821bbc2
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-04-22 21:35:44 +0530

    22/04/2024

--- calculator/src/components/inputField.js
+++
@@ -1,7 +0,0 @@
-import styles from "./inputfield.module.css"
-function InputField(){
-    return(
-        <input className={styles.inputfield} type="text" placeholder="Enter the value"></input>
-    )
-}
-export default InputField;
--- day2/src/components/errorMessage.js
+++
@@ -1,9 +0,0 @@
-let ErrorMessage=()=>{
-    let foodItems = ["Daal", "Milk", "Salad", "Rice", "Chilled Water"];
-    return(
-        <>
-{foodItems.length === 0 ? <h4>Kuch nhi hai bhai</h4>: null}; 
-        </>
-    )
-}
-export default ErrorMessage;
